83a2a95def80c5871739167cb1315d591db6db88
Hosseinreza <hrakbari77@gmail.com>
2022-04-14 00:54:16 +0430

    Add one to one relation and reverse

--- routes/api.php
+++ routes/api.php
@@ -138,3 +138,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/user/{id}/post',function($id){
     return User::find($id)->post;
 });
+
+Route::get('/post/{id}/user',function($id){
+    return Post::find($id)->user->name ; 
+});
