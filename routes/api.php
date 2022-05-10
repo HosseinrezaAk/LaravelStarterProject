3fc6c4b597282a8ff49bcc66d2cbb94c566e5429
Hosseinreza <hrakbari77@gmail.com>
2022-05-11 01:54:26 +0430

    Add many to many routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Role;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -30,4 +31,55 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 | Many TO Many Crud
 |-----------------------------------------------------------------------
 */
+
+Route::get('/create/',function (){
+    $user = User::find(1);
+    $role = new Role(['name'=>'Administrator']);
+    $user->roles()->save($role);
+});
+
+Route::get('/create/role/{roleName}',function ($roleName){
+    $newRole = new Role(['name'=>$roleName]);
+    $newRole->save();
+});
+
+Route::get('/read',function (){
+    $user = User::findorfail(1);
+    foreach ($user->roles as $role){
+        echo $role->name;
+    }
+});
+
+Route::get('/update/{user_id}/{roleName}', function ($user_id,$roleName){
+    $user = User::find($user_id);
+    $roles = $user->roles();
+    foreach($roles as $role){
+        $role->name = $roleName;
+        $role->save();
+    }
+
+});
+
+Route::get('/delete/{user_id}/{role_id}',function ($user_id,$role_id){
+    $user = User::find($user_id);
+    foreach($user->roles as $role){
+        $role->where('id',$role_id)->delete();
+    }
+});
+
+
+Route::get('/attach',function (){
+    $user = User::findorfail(1);
+    $user->roles()->attach(1);
+});
+
+Route::get('/detach',function (){
+    $user = User::findorfail(1);
+    $user->roles()->detach(1);
+});
+
+Route::get('/sync',function (){
+    $user = User::findorfail(1);
+    $user->roles()->sync([1]);
+});
 
