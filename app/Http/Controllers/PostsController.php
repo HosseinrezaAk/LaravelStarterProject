e6c9898a82fbe140b5f19c39423e65d91e444c77
Hosseinreza <hrakbari77@gmail.com>
2022-05-17 16:28:16 +0430

    Edit comment

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    // php artisan make:controller --resource PostsController
     /**
      * Display a listing of the resource.
      *
