bcb27097fa4f1194fecb416b8e97de32f851892d
Hosseinreza <hrakbari77@gmail.com>
2022-05-17 16:40:46 +0430

    Add 4 views

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostsController;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -227,3 +228,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 |-----------------------------------------------------------------------
 */
 
+Route::resource('/posts',PostsController::class);
