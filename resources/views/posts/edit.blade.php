1bd5f17e0e7dfce930d990f2a4653aa7abfcb4e4
Hosseinreza <hrakbari77@gmail.com>
2022-06-04 19:40:14 +0430

    Change edit to illuminate/html

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -5,12 +5,16 @@
 
 @section('content')
     <h1>Edit View</h1>
-    <form method="post" action="/api/posts/{{$post->id}}">
-        {{csrf_field()}}
-        <input type="hidden" name="_method" value="PUT">
-        <input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">
-        <input type="submit" name="submit" value="UPDATE">
-    </form>
+{{--    <form method="post" action="/api/posts/{{$post->id}}">--}}
+        {!! Form::model($post, ['method'=>'PATCH', 'route'=>['posts.update',$post->id]]) !!}
+            {{csrf_field()}}
+{{--        <input type="hidden" name="_method" value="PUT">--}}
+{{--        <input type="text" name="title" placeholder="Enter title" value="{{$post->title}}">--}}
+{{--        <input type="submit" name="submit" value="UPDATE">--}}
+            {!! Form::label('title','Title: ') !!}
+            {!! Form::text('title',null,['class'=>'form-control']) !!}
+            {!! Form::submit('Update Post',['class'=>"btn btn-info"]) !!}
+        {!! Form::close() !!}
 
     <form method="post" action="/api/posts/{{$post->id}}">
         <input type="hidden" name="_method" value="DELETE">
