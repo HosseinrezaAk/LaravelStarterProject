84621f657b08a110f2b3cf08bf460c8059458a48
Hosseinreza <hrakbari77@gmail.com>
2022-06-04 19:15:28 +0430

    Add with {{!!}}

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,10 +5,16 @@
 
 @section('content')
     <h1>Create View</h1>
-    <form method="post" action="/api/posts">
-        <input type="text" name="title" placeholder="Enter title" >
-        <input type="submit" name="submit">
-    </form>
+{{--    <form method="post" action="/api/posts">--}}
+    {!! Form::open(['method'=>'POST', 'route'=>'posts.store']) !!}
+
+        <div class="form-group">
+            {!! Form::label('title','Title: ') !!}
+            {!! Form::text('title',null,['class'=>'form-control']) !!}
+        </div>
+        {!! Form::submit('Create Post',['class'=>'btn btn-primary']) !!}
+        
+    {!! Form::close() !!}
 
 
 @endsection
